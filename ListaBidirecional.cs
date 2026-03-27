b70e453507f89ba7e4685146f76a3d91e59b7073
Vinicius <vinicius.nascc.silva@gmail.com>
2026-03-27 22:17:03 +0000

    Adicionar arquivos de projeto.

---
+++ ListaBidirecional.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lista_musicas
+{
+    public class Node
+    {
+        public string Value;
+        public Node Next;
+        public Node Previous;
+
+        public Node(string v)
+        {
+            // stores the node data (in this case, a string). It's the content the list stores and manages
+            Value = v;
+            // Reference to the next node in the list. If the node is the last one, this field is null
+            Next = null;
+            // Reference to the previous node in the list. If the node is the first one, this field is null
+            Previous = null;
+        }
+    }
+
+    public class DoublyLinkedList
+    {
+        // Reference to the first node in the list (head -> tail) using Next
+        private Node head; // Start of the list
+        // Reference to the last node in the list (tail -> head) using Previous
+        private Node tail; // End of the list
+        public Node current;
+
+        public DoublyLinkedList()
+        {
+            // both null, means empty list
+            head = null; // empty list
+            tail = null; // empty list
+        }
+
+        public void Insert(string value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+            current = newNode; // Sets the new node as the current one
+        }
+
+        // gets the current song
+        public string Musica_atual()
+        {
+            if (head == null)
+            {
+                return "No songs in the playlist.";
+            }
+            return head.Value;
+        }
+
+        // moves to the next song
+        public void Proxima_musica()
+        {
+            if (head != null && head.Next != null)
+            {
+                head = head.Next;
+            }
+        }
+
+        // goes back to the previous song
+        public void Musica_anterior()
+        {
+            if (head != null && head.Previous != null)
+            {
+                head = head.Previous;
+            }
+        }
+
+        // deletes a song
+        public void Deletar(string value)
+        {
+            Node currentNode = head;
+
+            // searches for the node with the value
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                {
+                    // Adjusts the previous reference
+                    if (currentNode.Previous != null)
+                        currentNode.Previous.Next = currentNode.Next;
+                    else
+                        head = currentNode.Next; // node was the first one
+
+                    // Adjusts the next reference
+                    if (currentNode.Next != null)
+                        currentNode.Next.Previous = currentNode.Previous;
+                    else
+                        tail = currentNode.Previous; // node was the last one
+
+                    // Adjusts current pointer if it's pointing to this node
+                    if (this.current == currentNode)
+                        this.current = currentNode.Next ?? currentNode.Previous;
+
+                    return; // exits the function after removing
+                }
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_musicas
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // basically the user control
+            int changeMusic;
+            // temporary variable for adding a song
+            string newSong;
+            // variable for bidirectional lists
+            DoublyLinkedList songs = new DoublyLinkedList();
+            // updates the current song
+            string currentSong;
+
+            // a pre-made song playlist
+            string[] greatPlaylist = new string[] {
+                "Bohemian Rhapsody",
+                "Billie Jean",
+                "Shape of You",
+                "Smells Like Teen Spirit",
+                "Imagine",
+                "Hotel California",
+                "Rolling in the Deep",
+                "Blinding Lights",
+                "Hey Jude",
+                "Uptown Funk"
+            };
+            // add each one
+            foreach (string song in greatPlaylist)
+            {
+                songs.Insert(song);
+            }
+
+            while (true)
+            {
+                // update current song
+                currentSong = songs.Musica_atual();
+                //
+                Console.WriteLine("Awesome Music App");
+
+                Console.WriteLine($"Now playing: {currentSong}\n");
+
+                Console.WriteLine("< - 1... 2 - >");
+                Console.WriteLine("3 - Delete song");
+                Console.WriteLine("4 - Add song");
+                Console.WriteLine("5 - Exit");
+                //
+
+                // Avoid errors that crash the console due to the variable being int
+                // if it's not an int it won't crash
+                try
+                {
+                    Console.Write(">");
+                    changeMusic = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Enter a value between 1 and 2...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+                // better than if for this situation
+                switch (changeMusic)
+                {
+                    // options, self-explanatory
+                    case 1:
+                        songs.Proxima_musica();
+                        break;
+
+                    case 2:
+                        songs.Musica_anterior();
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Are you sure you want to delete the current song? (Y - 1/N - 2)");
+
+                        changeMusic = Convert.ToInt32(Console.ReadLine());
+
+                        if (changeMusic == 1)
+                        {
+                            songs.Deletar(songs.Musica_atual());
+                        }
+
+                        break;
+
+                    case 4:
+
+                        Console.Write("\nWhich song to add?\n>");
+
+                        newSong = Console.ReadLine();
+
+                        songs.Insert(newSong);
+                        break;
+
+                    case 5:
+                        break;
+
+                    default:
+                        Console.WriteLine("Choose between 1 and 2...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+                if (changeMusic == 5) break;
+
+                Console.Clear();
+            }
+        }
+    }
+}
